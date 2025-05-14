57e543b3fae3097400e22f6881d8be0a1f22201e
weyltensor007 <weyltensor@gmail.com>
2025-05-14 11:31:32 +0800

    IS_GITHUB

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pandas
 import requests
 import pytz
-
+import os
 tz_taiwan = pytz.timezone('Asia/Taipei')
 
 n = random.randint(0,100)
@@ -12,5 +12,8 @@ outstr = f"{datetime.now(tz_taiwan)}__random:{n}"
 
 with open("log.txt", 'a') as f:
     f.write(outstr+"\n")
-    
+
+IS_GITHUB = os.getenv("GITHUB_ACTIONS") == "true"
+
 print(outstr)
+print(IS_GITHUB)
